7eaa7460d515549a660ce9f741da3086fb5b3d2f
Derpius <49565664+Derpius@users.noreply.github.com>
2024-10-20 21:40:44 +0100

    Implement compact surface parser

--- src/phy.cpp
+++ src/phy.cpp
@@ -21,7 +21,11 @@ namespace PhyParser {
     for (int i = 0; i < header.solidCount; i++) {
       const auto surfaceHeader = dataView.parseStruct<SurfaceHeader>(offset, "Failed to parse surface header").first;
 
-      solids.push_back(parseSurface(surfaceHeader, dataView.withOffset(offset + sizeof(SurfaceHeader))));
+      auto solidsForSurface = parseSurface(surfaceHeader, dataView.withOffset(offset + sizeof(SurfaceHeader)));
+      solids.insert(
+        solids.end(), std::make_move_iterator(solidsForSurface.begin()), std::make_move_iterator(solidsForSurface.end())
+      );
+
       offset += surfaceHeader.size + sizeof(SurfaceHeader::size);
     }
   }
@@ -30,28 +34,32 @@ namespace PhyParser {
     return header.checksum;
   }
 
-  Phy::Solid Phy::parseSurface(const SurfaceHeader& surfaceHeader, const OffsetDataView& data) {
-
-    // switch (surfaceHeader.modelType) {
-    //   case ModelType::IVPCompactSurface:
-    //     return parseCompactSurface(data);
-    //   case ModelType::IVPMOPP:
-    //     return parseMopp(data);
-    //   case ModelType::IVPBall:
-    //     throw InvalidBody("Unsupported surface model type IVPBall");
-    //   case ModelType::IVPVirtual:
-    //     throw InvalidBody("Unsupported surface model type IVPVirtual");
-    //   default:
-    //     throw InvalidBody("Unrecognised surface model type");
-    // }
+  const std::vector<Phy::Solid>& Phy::getSolids() const {
+    return solids;
   }
 
-  Phy::Solid Phy::parseCompactSurface(const OffsetDataView& data) {
+  std::vector<Phy::Solid> Phy::parseSurface(const SurfaceHeader& surfaceHeader, const OffsetDataView& data) {
+    switch (surfaceHeader.modelType) {
+      case ModelType::IVPCompactSurface:
+        return parseCompactSurface(data);
+      case ModelType::IVPMOPP:
+        return parseMopp(data);
+      case ModelType::IVPBall:
+        throw InvalidBody("Unsupported surface model type IVPBall");
+      case ModelType::IVPVirtual:
+        throw InvalidBody("Unsupported surface model type IVPVirtual");
+      default:
+        throw InvalidBody("Unrecognised surface model type");
+    }
+  }
+
+  std::vector<Phy::Solid> Phy::parseCompactSurface(const OffsetDataView& data) {
     const auto surfaceHeader =
       data.parseStruct<CompactSurfaceHeader>(0, "Failed to parse compact surface header").first;
     const auto nodeData =
       data.withOffset(offsetof(CompactSurfaceHeader, massCentre) + surfaceHeader.offsetLedgetreeRoot);
 
+    std::vector<Solid> solids;
     std::stack<size_t> nodeOffsets;
     nodeOffsets.push(0);
 
@@ -62,21 +70,22 @@ namespace PhyParser {
       if (node.isTerminal()) {
         const auto [ledge, ledgeOffset] =
           nodeData.parseStruct<Ledge>(nodeOffset + node.compactNodeOffset, "Failed to parse ledge");
-        parseLedge(ledge, data.withOffset(ledgeOffset)); // TODO: Return and store vertex + index data on Solid
+
+        solids.push_back(parseLedge(ledge, data.withOffset(ledgeOffset)));
       } else {
         nodeOffsets.push(nodeOffset + node.rightNodeOffset);
         nodeOffsets.push(nodeOffset + sizeof(LedgeNode));
       }
     }
+
+    return std::move(solids);
   }
 
-  Phy::Solid Phy::parseMopp(const OffsetDataView& /*data*/) {
+  std::vector<Phy::Solid> Phy::parseMopp(const OffsetDataView& /*data*/) {
     throw std::runtime_error("Not implemented");
   }
 
-  std::pair<std::vector<Vector4>, std::vector<uint16_t>> Phy::parseLedge(
-    const Ledge& ledge, const OffsetDataView& data
-  ) {
+  Phy::Solid Phy::parseLedge(const Ledge& ledge, const OffsetDataView& data) {
     const auto triangles = data.parseStructArrayWithoutOffsets<CompactTriangle>(
       sizeof(Ledge), ledge.trianglesCount, "Failed to parse triangle array"
     );
@@ -100,6 +109,6 @@ namespace PhyParser {
       ledge.pointOffset, maxVertexIndex + 1, "Failed to parse vertex array"
     );
 
-    return std::make_pair(std::move(vertices), std::move(indices));
+    return {.vertices = std::move(vertices), .indices = std::move(indices)};
   }
 }
--- src/phy.hpp
+++ src/phy.hpp
@@ -10,24 +10,29 @@
 namespace PhyParser {
   class Phy {
   public:
-    struct Solid {};
+    struct Solid {
+      std::vector<Structs::Vector4> vertices;
+      std::vector<uint16_t> indices;
+    };
 
     explicit Phy(const std::span<std::byte>& data, const std::optional<int64_t>& checksum = std::nullopt);
 
     [[nodiscard]] int64_t getChecksum() const;
 
+    [[nodiscard]] const std::vector<Solid>& getSolids() const;
+
   private:
     Structs::Header header;
     std::vector<Solid> solids;
 
-    [[nodiscard]] static Solid parseSurface(const Structs::SurfaceHeader& surfaceHeader, const OffsetDataView& data);
-
-    [[nodiscard]] static Solid parseCompactSurface(const OffsetDataView& data);
-
-    [[nodiscard]] static Solid parseMopp(const OffsetDataView& data);
-
-    [[nodiscard]] static std::pair<std::vector<Structs::Vector4>, std::vector<uint16_t>> parseLedge(
-      const Structs::Ledge& ledge, const OffsetDataView& data
+    [[nodiscard]] static std::vector<Solid> parseSurface(
+      const Structs::SurfaceHeader& surfaceHeader, const OffsetDataView& data
     );
+
+    [[nodiscard]] static std::vector<Solid> parseCompactSurface(const OffsetDataView& data);
+
+    [[nodiscard]] static std::vector<Solid> parseMopp(const OffsetDataView& data);
+
+    [[nodiscard]] static Solid parseLedge(const Structs::Ledge& ledge, const OffsetDataView& data);
   };
 }
